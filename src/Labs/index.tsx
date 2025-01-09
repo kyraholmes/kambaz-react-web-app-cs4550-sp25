93aeb949031a59dbbd351c636008e9da84925ec3
Kyra Holmes <kyra@holmesmail.com>
2025-01-08 20:05:55 -0500

    update to lecture

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,6 +5,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div>
+            <h1>Labs</h1>
             <Lab1></Lab1>
             <Lab2></Lab2>
             <Lab3></Lab3>
