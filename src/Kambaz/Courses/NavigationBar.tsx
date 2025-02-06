bedad05859907ad5fe90a5bc88a9a53679819ee7
Kyra Holmes <kyra@holmesmail.com>
2025-02-05 23:31:06 -0500

    A2 fixed

--- src/Kambaz/Courses/NavigationBar.tsx
+++ src/Kambaz/Courses/NavigationBar.tsx
@@ -1,7 +1,5 @@
 import { Container, Nav, Navbar} from "react-bootstrap";
-import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
-import { Offcanvas } from "react-bootstrap";
 
 export default function CourseNavigationBar() {
   return (
